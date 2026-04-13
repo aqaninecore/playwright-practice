0077d678499d2687416cbbb4a212925ff6f562a1
aqaninecore <yuri.sekachev@gmail.com>
2026-04-13 16:21:49 +0200

    workers and retries numbers update

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,19 +19,19 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'never' }], ['line'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'https://qauto.forstudy.space',
     httpCredentials: {
-      username: 'guest',
-      password: 'welcome2qauto',
-      // username: process.env.AUTH_USERNAME!,
-      // password: process.env.AUTH_PASSWORD!,
+      //username: 'guest',
+      //password: 'welcome2qauto',
+      username: process.env.AUTH_USERNAME!,
+      password: process.env.AUTH_PASSWORD!,
     },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
